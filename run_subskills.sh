1ba2b2f696a493fc0c2da0b925a41f5836cc405e
Paolo de Heer <paolodhr@gmail.com>
2018-01-12 18:06:20 +0100

    Added port information to sample run command

--- run_subskills.sh
+++ run_subskills.sh
@@ -4,4 +4,6 @@ cd C:\Github_Repos\tessler_Malmo_fork
 
 python main.py dqn subskills --double_dqn --normalize_reward --verbose_prints
 
-python main.py dqn subskills --double_dqn --normalize_reward --verbose_prints --malmo_ports 10001
+python main.py dqn subskills --double_dqn --normalize_reward --verbose_prints --malmo_ports 10001 --save_name skills_nav1
+
+python main.py dqn single_room --double_dqn --normalize_reward --verbose_prints --malmo_ports 10001
