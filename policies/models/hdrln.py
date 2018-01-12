1041f723c405c2cb43eb8afbff18c2e0ab5b5f04
Paolo de Heer <paolodhr@gmail.com>
2018-01-12 18:08:45 +0100

    Added a H-DRLN policy + parameter + model file. NB: The content of those files are currently modelled after the DQN and are not yet in working condition.

---
+++ policies/models/hdrln.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+# TODO: CHANGE FROM DQN TO H-DRLN
+class Flatten(nn.Module):
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
+
+
+class HDRLN(nn.Module):
+    """Implements a 3 layer convolutional network with 2 fully connected layers at the end as explained by:
+    Mnih et al. (2013) - https://arxiv.org/abs/1312.5602
+    """
+
+    def __init__(self, n_action: int, state_size: int):
+        super(HDRLN, self).__init__()
+        self.n_action = n_action
+
+        self.sequential_model = nn.Sequential(
+            nn.Conv2d(state_size, 32, kernel_size=8, stride=4, padding=0),  # (In Channel, Out Channel, ...)
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.n_action)
+        )
+
+    def forward(self, x):
+        return self.sequential_model(x)
