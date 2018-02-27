72b8fb3fdcaeec49338d8ea381eadaad36cc382e
Paolo de Heer <paolodhr@gmail.com>
2018-02-27 15:05:57 +0100

    Updated parameters

--- parameters/base_parameters.py
+++ parameters/base_parameters.py
@@ -10,23 +10,28 @@ parser.add_argument('--malmo_ports', type=int, nargs='+', default=None,
 parser.add_argument('--number_of_agents', type=int, default=1,
                     help='Number of agents to run in parallel, all use the same policy. This is used to increase the number of observation samples obtained over time.')
 parser.add_argument('--max_steps', type=int, default=2000000)
-parser.add_argument('--eval_frequency', type=int, default=1000)
-parser.add_argument('--eval_steps', type=int, default=100)
+parser.add_argument('--eval_frequency', type=int, default=20000,
+                    help='The interval of evaluation (test). Use 20000 for the separate subskill DSNs; 5000 for the 2- and 3-room domains.')
+parser.add_argument('--eval_steps', type=int, default=500,
+                    help='The amount of evaluation steps. Use 500 for separate subskill DSNs; 1000 for the 2- and 3-room domains.')
 parser.add_argument('--image_width', type=int, default=84)
 parser.add_argument('--image_height', type=int, default=84)
 parser.add_argument('--retain_rgb', default=False, action='store_true')
 parser.add_argument('--no_visualization', default=False, action='store_true',
                     help='When flag exists, will not plot visualizations.')
-parser.add_argument('--visualization_frequency', default=1, type=int,
+parser.add_argument('--visualization_frequency', default=10, type=int,
                     help='How often to plot visualizations.')
 parser.add_argument('--graph_moving_average_length', default=5, type=int,
                     help='Graphs plotted using a moving average over the last N points.')
 parser.add_argument('--verbose_prints', default=False, action='store_true',
                     help='Enable verbose debug prints for more informative details.')
-parser.add_argument('--ms_per_tick', default=100, type=int,
+parser.add_argument('--ms_per_tick', default=50, type=int,
                     help='Delay between ticks, this is a setting for the Malmo simulator.')
 parser.add_argument('--checkpoint_interval', type=int, default=5000)
 parser.add_argument('--save_name', type=str, help='The filename given to the saved checkpoint.')
 parser.add_argument('--no_cuda', default=False, action='store_true', help='When flag exists, cuda will not be used.')
 parser.add_argument('--resume', default=False, action='store_true',
                     help='Resume training of previous model based on save_name.')
+
+parser.add_argument('--experiment', default='pickup',
+                    help='Choose from the five subskills (nav1, nav2, pickup, place, break), or the \'tworoom\' or \'threeroom\' domain')
--- run_subskills.sh
+++ run_subskills.sh
@@ -9,4 +9,4 @@ python main.py dqn subskills --double_dqn --normalize_reward --verbose_prints --
 python main.py dqn single_room --double_dqn --normalize_reward --verbose_prints --malmo_ports 10001
 
 # subskill pickup
-python main.py dqn subskills --double_dqn --normalize_reward --verbose_prints --malmo_ports 10001 --save_name skills_pickup --experiment pickup --retain_rgb
+python main.py qr_dqn subskills --number_of_atoms 200 --number_of_agents 1 --malmo_ports 10000 --retain_rgb --save_name skills_pickup_qr --double_dqn --verbose_prints --experiment pickup --success_replay_memory
