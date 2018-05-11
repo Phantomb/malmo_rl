8e888f74a1aa5e7da0baa41f139547a70299613b
paolo <paolodhr@hotmail.com>
2018-05-11 09:22:03 +0200

    Return vis frequency to 1 to be able to spot bugs

--- parameters/base_parameters.py
+++ parameters/base_parameters.py
@@ -19,7 +19,7 @@ parser.add_argument('--image_height', type=int, default=84)
 parser.add_argument('--retain_rgb', default=False, action='store_true')
 parser.add_argument('--no_visualization', default=False, action='store_true',
                     help='When flag exists, will not plot visualizations.')
-parser.add_argument('--visualization_frequency', default=10, type=int,
+parser.add_argument('--visualization_frequency', default=1, type=int,
                     help='How often to plot visualizations.')
 parser.add_argument('--graph_moving_average_length', default=5, type=int,
                     help='Graphs plotted using a moving average over the last N points.')
